33d804c5ff890cd7228ce450513beb02356a6193
Viltu Jujhajiya <viltujujhajiya@gmail.com>
2025-04-05 11:55:18 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End-to-end pipeline for Churn Prediction using Machine Learning',
+    author='viltu-jujhajiya',
+    license='MIT',
+)
